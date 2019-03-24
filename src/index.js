d139dd3dd751c9ac84d135007448c6aa2cea9c04
Adam Anderson <adam.m.anderson@gmail.com>
2019-03-24 18:17:33 +0000

    testty

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import chalk from 'chalk'
 import { stopPointId, refresh } from '../config'
 import getStopPointArrivals from './helpers/get-stoppoint-arrivals'
 import formatArrivals from './helpers/format-arrivals'
-import scrollController from 'scroll-controller'
+import scrollController from './helpers/scroll-controller'
 
 const log = console.log
 log(chalk.bold.blue('WHERE\'S MY BUS'))
