fa8aaf4aaf334efd05dfbee5318e29d2d9b0cd4f
Adam Anderson <adam.m.anderson@gmail.com>
2019-03-24 18:06:52 +0000

    Add i2c-bus

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import chalk from 'chalk'
 import { stopPointId, refresh } from '../config'
 import getStopPointArrivals from './helpers/get-stoppoint-arrivals'
 import formatArrivals from './helpers/format-arrivals'
+import display from './helpers/scroll-controller'
 
 const log = console.log
 log(chalk.bold.blue('WHERE\'S MY BUS'))
@@ -10,6 +11,7 @@ setInterval(() => {
   getStopPointArrivals({ stopPointId })
     .then((arrivals) => {
       formatArrivals(arrivals)
+      display('hey')
     })
     .catch(() => {})
 }, 2000)
