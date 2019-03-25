13f4d2c35db3e57cae4fbafc23f056ab63d3737c
Adam Anderson <adam.m.anderson@gmail.com>
2019-03-25 00:55:07 +0000

    Tweaks to request status code

--- python/main.py
+++ python/main.py
@@ -2,6 +2,7 @@ import time
 import asyncio
 import threading
 import scrollphathd
+from scrollphathd.fonts import font3x5
 from os import getenv
 import requests
 from math import floor
@@ -17,6 +18,22 @@ def seconds_to_minutes(seconds):
 def format_minutes(minutes):
     return f'{minutes}m' if minutes > 0 else 'due'
 
+def scroll_message(message):
+        scrollphathd.clear()
+        length = scrollphathd.write_string(message, x=1)
+        scrollphathd.show()
+        time.sleep(0.5)
+
+        length -= scrollphathd.width
+
+        while length > 0:
+            scrollphathd.scroll(1)
+            scrollphathd.show()
+            length -= 1
+            time.sleep(0.02)
+
+        time.sleep(0.5)
+
 def setInterval(func,time):
     e = threading.Event()
     while not e.wait(time):
@@ -26,40 +43,32 @@ async def get_stoppoint_arrivals():
     print('Fetching StopPoint Arrivals')
     endpoint = f'https://api.tfl.gov.uk/StopPoint/{getenv("STOPPOINT_ID")}/Arrivals'
     request = requests.get(endpoint, params={'app_id': getenv("API_APP_ID"), 'app_key': getenv("API_APP_KEY")})
-    return request.json()
-
+    if request.status_code == 200:
+        return request.json()
+    else:
+        print(f'error code {request.status_code}')
+        scroll_message(f'error {request.status_code}')
+        return []
 
 async def format_arrivals(arrivals):
-    if len(arrivals) < 1:
-        return 'No arrivals due'
+    if not arrivals:
+        return 'No arrivals'
 
     line_name = arrivals[0]['lineName']
     arrivals.sort(key=lambda k: k['timeToStation'], reverse=True)
     mapped_arrivals = map(lambda x: format_minutes(seconds_to_minutes(x['timeToStation'])), arrivals)
-    joined_arrivals = ' | '.join(mapped_arrivals)
+    joined_arrivals = ' '.join(mapped_arrivals)
     
     print(joined_arrivals)
     return f'{line_name} {joined_arrivals}'
 
 async def display_arrivals(value):
     print('Displaying arrivals')
-    scrollphathd.clear()
-    length = scrollphathd.write_string(value)
-    scrollphathd.show()
-    time.sleep(0.5)
-    length -= scrollphathd.width
-
-    while length > 0:
-        scrollphathd.scroll(1)
-        scrollphathd.show()
-        length -= 1
-        time.sleep(0.04)
-
-    time.sleep(0.5)
+    scroll_message(value)
+    time.sleep(5)
 
 async def main():
     while True:
-        await asyncio.sleep(3)
         await display_arrivals(
             await format_arrivals(
                 await get_stoppoint_arrivals()
